4300a8daed06afd56391aa3a7f83657d8c07a8bb
Ethan Sanders <ethanolsanders@gmail.com>
2026-01-11 16:36:11 -0600

    Giving a link for other ethan

--- src/main/java/frc/robot/commands/auto/AutoAlign.java
+++ src/main/java/frc/robot/commands/auto/AutoAlign.java
@@ -3,7 +3,7 @@ package frc.robot.commands.auto;
 import frc.robot.subsystems.swervedrive.SwerveSubsystem;
 
 public class AutoAlign {
-
+    // https://github.com/team1306/Robot2024/blob/main/src/main/java/frc/robot/commands/drive/ShooterDriveCommand.java
     private SwerveSubsystem swerve;
     
 }
