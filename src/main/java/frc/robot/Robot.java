2a2a2eebe2c7d5b648f2c6c3d8ce8322f89bf861
Ethan Sanders <ethanolsanders@gmail.com>
2026-01-11 10:19:15 -0600

    implement badgerlog

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,11 +4,13 @@
 
 package frc.robot;
 
+import badgerlog.BadgerLog;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to each mode, as
@@ -70,6 +72,7 @@ public class Robot extends TimedRobot
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
+    BadgerLog.update(); // handles logic for SmartDashboard library Ethan is using
   }
 
   /**
